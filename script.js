e9715b974206c8867407c2bd34be079ab3706e04
abdulhaseebcv <abdulcvhaseeb@gmail.com>
2022-12-11 01:39:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var screen = document.getElementById("outputfield")
+
+function buttonclick(val) {
+    screen.value += val
+}
+
+function equalClicked() {
+    var text = screen.value
+    var result = eval(text)
+    screen.value = result
+}
+
+function cleardisplay() {
+    screen.value = ""
+}
+
+function deletebtn() {
+    screen.value = screen.value.slice(0,-1)
+}
+
+
